f1fc885b600e7660d13c58ccfc156fe7d1a1e475
Omkar Patil <Omkarp5429@gmail.com>
2022-06-28 21:08:48 +0530

    javaScript updated

---
+++ triangleCheck.js
@@ -0,0 +1,34 @@
+const btnCheck = document.querySelector(".btn-check");
+const anglesAll = document.querySelectorAll(".angle-input")
+const finalMessage = document.querySelector(".final-message")
+
+function inputCalculator(){
+const angleOne = anglesAll[0].value;
+const angleTwo = anglesAll[1].value;
+const angleThree = anglesAll[2].value;
+const sum = sumCalculator(angleOne,angleTwo,angleThree);
+checker(sum)
+}
+
+function sumCalculator(angleOne,angleTwo,angleThree){
+  if(angleOne!=0, angleTwo!=0, angleThree!=0, angleOne&&angleTwo&&angleThree){
+    const  sumOfAngles = Number(angleOne) + Number(angleTwo) + Number(angleThree);
+    return sumOfAngles; 
+  }
+else{
+    finalMessage.innerText= "It is not a Triangle"
+}
+
+    }
+
+
+function checker(sum){
+    if (sum === 180) {
+       finalMessage.innerText= "It is a Triangle"
+    }
+    else{
+        finalMessage.innerText= "It is not a Triangle"
+    }
+}
+
+btnCheck.addEventListener("click", inputCalculator)
